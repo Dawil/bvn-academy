5300e717e22e1cb766384a7f613dfe518ffa5c36
dave <dave@davidgwilcox.com>
2012-12-21 14:49:48 +1100

    added code to select a quiz to display when the video is paused

--- src/controllers/video_controller.js
+++ src/controllers/video_controller.js
@@ -2,15 +2,50 @@ var Controllers;
 (function (Controllers) {
     Controllers.VideoController = function ($scope, youtube) {
         $scope.videoURL = "9bZkp7q19f0";
+        $scope.quizzes = [
+            {
+                question: "Select the correct option:",
+                options: [
+                    "Correct", 
+                    "Incorrect"
+                ],
+                correctOption: "Correct",
+                time: 3
+            }, 
+            {
+                question: "How many options are there?",
+                options: [
+                    "One"
+                ],
+                correctOption: "One",
+                time: 7
+            }, 
+            {
+                question: "What 'style' is this video?",
+                options: [
+                    "Gangnam Style", 
+                    "Oppa Style", 
+                    "K-POP Style", 
+                    "BVN Donnovan Hill Style"
+                ],
+                correctOption: "Gangnam Style",
+                time: 5
+            }
+        ];
         youtube.load($scope.videoURL, 'ytplayer').done(function () {
             youtube.getPlayer().playVideo();
             youtube.getPlayer().mute();
         });
-        youtube.atSecond(3, function () {
-            $scope.showVideo = false;
-            youtube.getPlayer().pauseVideo();
-            $scope.$digest();
+        $scope.quizzes.map(function (quiz) {
+            youtube.atSecond(quiz.time, function () {
+                $scope.selectedQuiz = quiz;
+                $scope.showVideo = false;
+                youtube.getPlayer().pauseVideo();
+                $scope.$digest();
+            });
         });
+        $scope.attemptQuiz = function () {
+        };
         $scope.showVideo = true;
         $scope.switchMode = function () {
             $scope.showVideo = !$scope.showVideo;
--- src/controllers/video_controller.ts
+++ src/controllers/video_controller.ts
@@ -4,29 +4,69 @@
 
 module Controllers {
 
-	interface IVideoScope extends ng.IScope {
+	export interface IVideoScope extends ng.IScope {
 		videoURL:string;
 		showVideo:bool;
 		switchMode:()=>void;
+		quizzes: Quiz[];
+		selectedQuiz: Quiz;
+		selectedOption: string;
+		attemptQuiz: () => void;
 	}
 
-	export var VideoController:any = function($scope:IVideoScope, youtube:Services.IYoutubeService) {
+	export interface Quiz {
+		question:string;
+		options:string[];
+		correctOption:string;
+		time:number;
+	}
+
+	export var VideoController:any = function($scope:IVideoScope,
+			youtube:Services.IYoutubeService) {
 		$scope.videoURL = "9bZkp7q19f0";
+		$scope.quizzes = [
+				{
+					question: "Select the correct option:",
+					options: ["Correct","Incorrect"],
+					correctOption: "Correct",
+					time: 3
+				},
+				{
+					question: "How many options are there?",
+					options: ["One"],
+					correctOption: "One",
+					time: 7
+				},
+				{
+					question: "What 'style' is this video?",
+					options: ["Gangnam Style", "Oppa Style", "K-POP Style",
+						"BVN Donnovan Hill Style"],
+					correctOption: "Gangnam Style",
+					time: 12
+				}
+			];
 		youtube.load($scope.videoURL, 'ytplayer')
 			.done(()=>{
 				youtube.getPlayer().playVideo();
 				youtube.getPlayer().mute();
 			});
-		youtube.atSecond(3,()=>{
-			$scope.showVideo = false;
-			youtube.getPlayer().pauseVideo();
-			$scope.$digest();
+		$scope.quizzes.map((quiz:Quiz):void => {
+			youtube.atSecond(quiz.time,()=>{
+				$scope.selectedQuiz = quiz;
+				$scope.showVideo = false;
+				youtube.getPlayer().pauseVideo();
+
+				$scope.$digest();
+			});
 		});
+		$scope.attemptQuiz = () => {
+
+		};
 		$scope.showVideo = true;
 		$scope.switchMode = () => {
 			$scope.showVideo = !$scope.showVideo;
 		};
-	}
+	};
 	VideoController.$inject = ['$scope', 'youtube'];
 
 }
