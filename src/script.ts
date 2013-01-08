e577f416a7f2986c94221d9d46bcf67d03edcf83
dave <dave@davidgwilcox.com>
2013-01-08 11:05:56 +1100

    removed redundant src/script file

--- src/script.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../declarations/jquery-1.8.d.ts" />
-/// <reference path="../declarations/youtube.d.ts" />
-/// <reference path="../declarations/angular-1.0.d.ts" />
-/// <reference path="timer.ts" />
-/// <reference path="services/youtube_service.ts" />
-/// <reference path="controllers/video_controller.ts" />
-
-
